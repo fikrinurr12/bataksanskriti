f0bdfa93acbcb9d3e5074b85bd9ff03054092b53
Aan Alma <aanalma03@gmail.com>
2023-12-31 13:02:24 +0700

    membuat template halaman dashboard

--- resources/views/layout/landingpage.blade.php
+++ resources/views/layout/landingpage.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- css & js -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
     @vite(['resources/sass/app.scss','resources/js/app.js'])
     <!-- css & js -->
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LandingPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use App\Http\Controllers\RegisterController;
 |
 */
 
-Route::get('/',[LandingPageController::class, 'index'])->name('landingpage');
+Route::get('/', [LandingPageController::class, 'index'])->name('landingpage');
 Route::get('/modul',[LandingPageController::class, 'modul'])->name('modul');
 Route::get('/modul/content',[LandingPageController::class, 'content'])->name('isi_modul');
 
@@ -24,3 +25,6 @@ Route::get('/jadwal',[LandingPageController::class, 'jadwal'])->name('jadwal');
 
 Route::get('/login',[LoginController::class, 'index'])->name('login');
 Route::get('/register',[RegisterController::class, 'index'])->name('register');
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
