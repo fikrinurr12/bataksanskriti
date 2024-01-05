6cc45188366659862d6c55f82a9866e086c129bc
fikrinurr12 <fikri030504@gmail.com>
2024-01-05 09:57:48 +0700

    fix login

--- resources/views/form/login.blade.php
+++ resources/views/form/login.blade.php
@@ -23,7 +23,7 @@
               </select>
           </div> --}}
           <div class="text-center"><button type="submit" class="btn btn-danger px-5 mb-5 w-100">Login</button></div>
-          <div id="emailHelp" class="form-text text-center mb-5 text-dark">Belum Registrasi? <a href="{{ route('register') }}" class="text-dark fw-bold"> Buat Akun</a>
+          <div id="emailHelp" class="form-text text-center mb-5 text-dark">Belum Registrasi? <a href="{{ route('register') }}" class="text-dark fw-bold"> Buat Akun Sekarang</a>
           </div>
         </form>
       </div>
