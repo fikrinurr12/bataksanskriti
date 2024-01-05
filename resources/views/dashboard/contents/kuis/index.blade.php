e73362a31d9acac1d40f3a564f94da0f1850adc8
fikrinurr12 <fikri030504@gmail.com>
2024-01-05 22:16:36 +0700

    mengganti informasi jawaban ke informasi skor di kuis

--- resources/views/dashboard/contents/kuis/index.blade.php
+++ resources/views/dashboard/contents/kuis/index.blade.php
@@ -1,5 +1,5 @@
 <a href="/modul" class="px-5 py-2 btn btn-danger mt-3">Buat Kuis</a>
-<button type='button' class='px-5 py-2 btn btn-primary mt-3' data-bs-toggle='modal' data-bs-target='#detail-jawaban'>Lihat Jawaban</button>
+<button type='button' class='px-5 py-2 btn btn-primary mt-3' data-bs-toggle='modal' data-bs-target='#detail-skor'>Lihat Skor</button>
 <div class="row row-cols-1 row-cols-md-3 g-4 mt-3 me-1">
   @foreach($datas as $data)
   <div class="col-sm">
@@ -16,11 +16,11 @@
   </div>
   @endforeach
 
-  <div class="modal fade" id="detail-jawaban" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="detail-skor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
       <div class="modal-content">
           <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Informasi Jawaban</h1>
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Informasi Skor</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
